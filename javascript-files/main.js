f07c932a83847fb75b0de5cc34de678fa19658ba
nottohave <nottohave@gmail.com>
2022-02-08 18:41:48 -0800

    update navigation

--- javascript-files/main.js
+++ javascript-files/main.js
@@ -3,16 +3,16 @@ var mobileNav = document.querySelector(".mobile-nav-toggle");
 
 // trying out hamburger button
 mobileNav.addEventListener("click", function() {
-    // upon click, get the value of data-visible
     var visibleValue = primeNav.getAttribute("data-visible");
-    console.log(visibleValue);
-    // change the value of data-visible to true to open nav
-    if (visibleValue === "false") {
+
+    // consider undefined case
+    if (visibleValue !== "true") {
         primeNav.setAttribute("data-visible", true);
-        console.log("true");
+        mobileNav.setAttribute("aria-expanded", true);
     } else {
         primeNav.setAttribute("data-visible", false);
-        console.log("false");
+        mobileNav.setAttribute("aria-expanded", false);
     }
-    // do the opposite afterward
+
 })
+
