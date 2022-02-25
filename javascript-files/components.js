ac4e06f1a330ad668489464bfa3bee642bac1917
nottohave <nottohave@gmail.com>
2022-02-25 09:56:10 -0800

    update components

--- javascript-files/components.js
+++ javascript-files/components.js
@@ -1,5 +1,6 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tab = tabList.querySelectorAll('[role="tab"]');
+var planetInfos = document.querySelectorAll(".planet_infos");
 
 const slideLeft = 37;
 const slideRight = 39;
@@ -7,11 +8,17 @@ const spaceBar = 32;
 
 let tabMe = 0;
 
+// create click event on the tab that user click on
+tab.forEach(function(tab) {
+    tab.addEventListener('click', moveTab)
+});
+
 // slide left and right with arrow keys
 tabList.addEventListener('keydown', switchTab);
 
-function switchTab(e) {
-    const keyPress = e.keyCode;
+// function for keyboard switching on tab control
+function switchTab(key) {
+    const keyPress = key.keyCode;
 
     // change the tabindex of the current tab to -1 
     tab.tabIndex = "-1";
@@ -40,3 +47,21 @@ function switchTab(e) {
     }
 }
 
+// Changing tab content upon user's click
+function moveTab(click) {
+    const targetClick = click.target;
+    let movingClick = targetClick.getAttribute("aria-controls");
+
+    for (i = 0; i < planetInfos.length; i++) {
+        planetInfos[i].setAttribute("hidden", "true");
+    }
+
+    const tabContainer = targetClick.parentNode;
+    const contentContainer = tabContainer.parentNode;
+
+    // when user click on said-button, get to the parent of that node and display that container
+    // hide the nonactive container
+    contentContainer.querySelector([`#${movingClick}`]).removeAttribute('hidden');
+
+    
+}
